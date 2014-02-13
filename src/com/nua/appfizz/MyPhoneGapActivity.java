f3ea9130e44520e28c0a53f064cd3036573cf374
Roger Lichfield <git@whootin.com>
2014-02-13 16:41:23 -0700

    MyAppBuilder.com App Fizz Android

---
+++ src/com/nua/appfizz/MyPhoneGapActivity.java
@@ -0,0 +1,38 @@
+package com.nua.appfizz;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.cordova.DroidGap;
+
+import android.annotation.SuppressLint;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@SuppressLint("NewApi")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 try {
+		        PackageInfo info = getPackageManager().getPackageInfo(
+		                "com.nua.appfizz", 
+		                PackageManager.GET_SIGNATURES);
+		        for (Signature signature : info.signatures) {
+		            MessageDigest md = MessageDigest.getInstance("SHA");
+		            md.update(signature.toByteArray());
+		            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+		            }
+		    } catch (NameNotFoundException e) {
+
+		    } catch (NoSuchAlgorithmException e) {
+
+		    }
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
